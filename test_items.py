78cb9861bbf05340d1d6ce35cc1b7df229782a55
andrey <a@test.com>
2019-07-26 10:30:45 +0300

    rename test to test_guest_check_basket_button

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_check_basket_button(browser):
     browser.get(link)
     browser.find_element_by_css_selector("button.btn-add-to-basket")
     time.sleep(30)
